80105115fb874fe91fd483c261f64840673bfa46
Aafaq Ali <ali@techless.com>
2022-06-24 00:38:58 +0500

    Fix: Used Coroutine insted of timer for network status ribbon delay

--- app/src/main/java/com/sadapay/takehomeexercise/features/trending_repositories/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/sadapay/takehomeexercise/features/trending_repositories/presentation/activities/MainActivity.kt
@@ -3,12 +3,11 @@ package com.sadapay.takehomeexercise.features.trending_repositories.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.sadapay.app_utils.utils.ConnectionLiveData
 import com.sadapay.takehomeexercise.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
-import java.util.*
-import kotlin.concurrent.timerTask
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -36,11 +35,12 @@ class MainActivity : AppCompatActivity() {
                 binding.textViewConnectivityAvailable.visibility = View.VISIBLE
                 binding.textViewNoConnectivity.visibility = View.GONE
 
-                Timer().schedule(timerTask {
+                lifecycleScope.launch {
+                    delay(3000)
                     GlobalScope.launch(Dispatchers.Main) {
                         binding.textViewConnectivityAvailable.visibility = View.GONE
                     }
-                }, 3000)
+                }
             } else {
                 binding.textViewConnectivityAvailable.visibility = View.GONE
                 binding.textViewNoConnectivity.visibility = View.VISIBLE
