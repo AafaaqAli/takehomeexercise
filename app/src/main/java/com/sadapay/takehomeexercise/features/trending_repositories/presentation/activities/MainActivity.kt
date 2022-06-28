6a0c1dcb00612db3a3241afdb9e5c23e3b5ded45
Aafaq Ali <ali@techless.com>
2022-06-28 12:50:12 +0500

    Implementation: Added initial darkmode configs

--- app/src/main/java/com/sadapay/takehomeexercise/features/trending_repositories/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/sadapay/takehomeexercise/features/trending_repositories/presentation/activities/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.sadapay.takehomeexercise.features.trending_repositories.presentation.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sadapay.app_utils.utils.network.ConnectionLiveData
-import com.sadapay.takehomeexercise.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.sadapay.app_utils.animation_utils.NetworkRibbonAnimationHelper
+import com.sadapay.app_utils.utils.network.ConnectionLiveData
+import com.sadapay.takehomeexercise.R
+import com.sadapay.takehomeexercise.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -22,6 +26,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        handleMenuClick()
+    }
+
+    private fun handleMenuClick() {
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            if (menuItem.itemId == R.id.menu_item_switch_ui_mode) {
+                Toast.makeText(this, "Under Progress...", Toast.LENGTH_SHORT).show()
+                return@setOnMenuItemClickListener true
+            } else {
+                return@setOnMenuItemClickListener false
+            }
+        }
     }
 
     @OptIn(DelicateCoroutinesApi::class)
@@ -32,9 +49,5 @@ class MainActivity : AppCompatActivity() {
         connectionLiveData.observe(this) { isNetworkAvailable ->
             NetworkRibbonAnimationHelper.animateRibbon(binding, isNetworkAvailable)
         }
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
